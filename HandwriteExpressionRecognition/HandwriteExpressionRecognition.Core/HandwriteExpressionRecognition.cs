c05453b66663f86a863ffa7a88fbf8503edaa343
Daniil Proskurka <dddddddaniil@gmail.com>
2017-04-20 22:45:03 +0300

    connect emgucv without testing

--- HandwriteExpressionRecognition/HandwriteExpressionRecognition.Core/HandwriteExpressionRecognition.cs
+++ HandwriteExpressionRecognition/HandwriteExpressionRecognition.Core/HandwriteExpressionRecognition.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
 
 namespace HandwriteExpressionRecognition.Core
 {
     public class HandwriteExpressionRecognition
     {
+        public void Temp()
+        {
+            
+        }
     }
 }
